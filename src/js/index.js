f4a1a46544b6652f063681ba2b1c8a1dad7c51f7
lwx344455 <liangqiong@huawei.com>
2016-12-15 15:12:22 +0800

    change user number to user name

--- src/js/index.js
+++ src/js/index.js
@@ -1,32 +1,50 @@
 angular.module('myApp', [
-    'service.sock'
+    'service.sock',
+    'ui.router'
 ])
-    .controller('mainCtrl', function ($scope, $interval, sockService) {
-        let value = null;
-        let btn = document.querySelector('.btn');
-        let input = document.querySelector('.text');
-        let content = document.querySelector('.content');
-        $interval(() => {
-            sockService.deferred.then(res => {
-                let div = document.createElement('div');
-                let text = res.pop();
-                if (text) {
-                    div.innerHTML = text;
-                    content.appendChild(div);
-                }
-                input.onkeyup = (e) => {
-                    if (e.keyCode == 13) {
-                        sendDataToBackend(input);
-                    }
-                };
-                btn.onclick = () => {
-                    sendDataToBackend(input);
-                };
-            });
-        }, 1000);
+    .config(($stateProvider) => {
+        $stateProvider
+            .state('home', {
+                url: '/home',
+                templateUrl: 'views/home.html',
+                controller: function($scope, $interval, sockService){
+                    let value = null;
+                    let btn = document.querySelector('.send-btn');
+                    let input = document.querySelector('.text');
+                    let content = document.querySelector('.content');
+                    $scope.hideLogin = ()=>{
 
-        function sendDataToBackend(input) {
-            sockService.ws.send(input.value);
-            input.value = '';
-        }
+                    };
+                    $interval(() => {
+                        sockService.deferred.then(res => {
+                            let div = document.createElement('div');
+                            let text = res.pop();
+                            if (text) {
+                                div.innerHTML = text;
+                                content.appendChild(div);
+                            }
+                            input.onkeyup = (e) => {
+                                if (e.keyCode == 13) {
+                                    sendDataToBackend(input);
+                                }
+                            };
+                            btn.onclick = () => {
+                                sendDataToBackend(input);
+                            };
+                        });
+                    }, 1000);
+
+                    function sendDataToBackend(input) {
+                        sockService.ws.send({value:input.value});
+                        input.value = '';
+                    }
+                }
+            })
+    })
+    .controller('mainCtrl', function ($scope, $interval, sockService) {
+        $scope.login = ()=>{
+            $scope.isHide = true;
+            let name = document.getElementById('username').value;
+            sockService.ws.send({name: name});
+        };
     })
